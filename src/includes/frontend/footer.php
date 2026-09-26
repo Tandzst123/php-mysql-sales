e5055e4e98ee1757368f261f760fc26fdc254eae
MINH TAN <tanvipst30@gmail.com>
2026-09-26 09:40:27 +0700

    Complete Hands-on 09 product storefront and detail

---
+++ src/includes/frontend/footer.php
@@ -0,0 +1,8 @@
+<footer class="mt-5 p-4 bg-dark text-white text-center">
+    <p class="mb-0">
+       Phone Accessories Management – Quản lý bán phụ kiện điện thoại
+    </p>
+</footer>
+
+</body>
+</html>
---
+++ src/includes/frontend/navbar.php
@@ -0,0 +1,65 @@
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">Sales Management</a>
+
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mainNavbar"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="mainNavbar">
+            <ul class="navbar-nav">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Trang chủ</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/categories/">
+                        Danh mục
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/products/">
+                        Sản phẩm
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        Đơn hàng
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/shippers">
+                        Shipper
+                    </a>
+                </li>
+
+                 <li class="nav-item">
+                    <a class="nav-link" href="/suppliers">
+                        Quản lý nhà cung cấp
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/customers">
+                        Quản lý khách hàng
+                    </a>
+                </li>
+
+                 <li class="nav-item">
+                    <a class="nav-link" href="/employees">
+                        Quản lý nhân viên
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
